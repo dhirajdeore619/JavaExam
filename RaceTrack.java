1f9494d6d1fb34f12d4dbf4d107bd4deda5736aa
dhirajdeore619 <103194286+dhirajdeore619@users.noreply.github.com>
2022-05-18 13:36:21 +0530

    Add files via upload

---
+++ RaceTrack.java
@@ -0,0 +1,14 @@
+class RaceTrack{
+	public static void main (String [] args){
+		Car c1  = new Car (2010,"Tata",40.0) ;
+		
+		c1.getSpeed();
+		c1.getYear();
+		c1.getMake();
+		
+		c1.accelerate();
+		
+		c1.getSpeed();
+		
+	}
+}
